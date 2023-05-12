868ebef0de57463b1cdd3f87afb2f4c81491c60a
Rodney Lafuente Mercado <rodneyslafuente@gmail.com>
2023-05-12 17:29:26 +0000

    further test

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # GPU Accelerated Vector Database
 
-A simple vector database that supports insertion and K Nearest Neighbors search. The only supported vector is a 64-bit binary vector for simplicity of implementation and usage of space.
+A Proof-of-Concept vector database that supports insertion and K Nearest Neighbors search. The only supported vector is a 64-bit binary vector for simplicity of implementation and usage of space.
 
 While this implementation can only support 64-bit binary vector, it can be extended to support any vector type, including vectors of non-binary elements such as floating point values. Such an extension would not require any alteration to the existing radix select implementation, only to the computation of distances.
 
--- testVectorDB.cu
+++ testVectorDB.cu
@@ -28,21 +28,22 @@ int main(void) {
   using boost::uuids::uuid;
 
   const char *vdbName = "test.txt";
+  int vdbCapacity = 1000;
 
   // open vector db
-  printf("Opening...");
+  printf("Opening...\n");
   auto t1 = high_resolution_clock::now();
-  VectorDB vdb(vdbName, 950000000);
+  VectorDB *vdb = new VectorDB(vdbName, vdbCapacity);
   auto t2 = high_resolution_clock::now();
   auto ms_int = duration_cast<milliseconds>(t2 - t1);
-  printf(" Done. Execution time: %ldms.\n", ms_int.count());
+  printf("Done. Execution time: %ldms.\n", ms_int.count());
 
   // TODO: Do generation-insertion in batches, not just insertion
 
   // generate random ids and vectors
-  int numToAdd = 50;
+  int numToAdd = 100;
   // use heap since these arrays are huge
-  printf("Generating...");
+  printf("Generating..\n");
   t1 = high_resolution_clock::now();
   uuid *ids = (uuid *)malloc(numToAdd * sizeof(uuid));
   uint64_cu *vectorsToAdd = (uint64_cu *)malloc(numToAdd * sizeof(uint64_cu));
@@ -52,10 +53,10 @@ int main(void) {
   }
   t2 = high_resolution_clock::now();
   ms_int = duration_cast<milliseconds>(t2 - t1);
-  printf(" Done. Execution time: %ldms.\n", ms_int.count());
+  printf("Done. Execution time: %ldms.\n", ms_int.count());
 
   // insert random ids and vectors
-  printf("Inserting...");
+  printf("Inserting...\n");
   t1 = high_resolution_clock::now();
   int chunkSize = 4 << 20;
   int numChunks = (numToAdd + chunkSize - 1) / chunkSize;
@@ -65,11 +66,11 @@ int main(void) {
     if (i == numChunks - 1) {
       numInChunk = numToAdd % chunkSize;
     }
-    vdb.insert(numInChunk, ids + start, vectorsToAdd + start);
+    vdb->insert(numInChunk, ids + start, vectorsToAdd + start);
   }
   t2 = high_resolution_clock::now();
   ms_int = duration_cast<milliseconds>(t2 - t1);
-  printf(" Done. Execution time: %ldms.\n", ms_int.count());
+  printf("Done. Execution time: %ldms.\n", ms_int.count());
 
   free(ids);
   free(vectorsToAdd);
@@ -81,12 +82,12 @@ int main(void) {
   float kNearestDistances[k];
   uuid kNearestIds[k];
 
-  printf("Querying...");
+  printf("Querying...\n");
   t1 = high_resolution_clock::now();
-  vdb.query(queryVector, k, kNearestDistances, kNearestVectors, kNearestIds);
+  vdb->query(queryVector, k, kNearestDistances, kNearestVectors, kNearestIds);
   t2 = high_resolution_clock::now();
   ms_int = duration_cast<milliseconds>(t2 - t1);
-  printf(" Done. Execution time: %ldms.\n", ms_int.count());
+  printf("Done. Execution time: %ldms.\n", ms_int.count());
 
   // print results
   printf("Query: ");
@@ -96,6 +97,22 @@ int main(void) {
            kNearestDistances[i]);
     printBits(kNearestVectors[i]);
   }
+
+  // close db
+  delete vdb;
+
+  // reopen
+  printf("Reopening...\n");
+  t1 = high_resolution_clock::now();
+  VectorDB *vdb2 = new VectorDB(vdbName, vdbCapacity);
+  t2 = high_resolution_clock::now();
+  ms_int = duration_cast<milliseconds>(t2 - t1);
+  printf("Done. Execution time: %ldms.\n", ms_int.count());
+
+  // query again
+
+  // close second db
+  delete vdb2;
 
   // delete file
   std::remove(vdbName);
